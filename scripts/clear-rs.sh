0f5c7d5ebbcf0cd87eab2e2b6de8fe40cf159820
Aleksander Okon <aleksander.okon@medicover.com>
2022-10-13 11:37:35 +0200

    now works

--- scripts/clear-rs.sh
+++ scripts/clear-rs.sh
@@ -1,3 +1,3 @@
 
 #! /bin/bash 
-kubectl get replicasets -A | awk '{if ($3 + $4 + $5 == 0) print $1 $2}' | xargs -l bash -c 'kubectl delete replicaset -n $0 $1'
+kubectl get replicasets -A | awk '{if ($3 + $4 + $5 == 0) print $1 " " $2}' | xargs -l bash -c 'kubectl delete replicaset -n $0 $1'
