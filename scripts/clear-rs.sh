01041c3d0ac862e8b2c2731c5b89a8ccbad8f236
Aleksander Okon <aleksander.okon@medicover.com>
2022-10-13 12:20:46 +0200

    add comments

--- scripts/clear-rs.sh
+++ scripts/clear-rs.sh
@@ -1,3 +1,7 @@
 
-#! /bin/bash 
-kubectl get replicasets -A | awk '{if ($3 + $4 + $5 == 0) print $1 " " $2}' | xargs -l bash -c 'kubectl delete replicaset -n $0 $1'
+#! /bin/bash
+#
+# Author: Aleksander Okon
+# Delete completed replicasets from all namespaces
+
+kubectl get replicasets -A | awk '{if ($3 + $4 + $5 == 0) print $1"\t"$2}' | xargs -l bash -c 'kubectl delete replicaset $1 -n $0'
