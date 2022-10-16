54a06e7b78c0fcba7170075be53cbd6f11c1560d
Aleksander Okon <aleksander.okon@medicover.com>
2022-10-16 18:47:12 +0200

    ingress for k8s dashboard http

--- docs/kubernetes-cluster-init.sh
+++ docs/kubernetes-cluster-init.sh
@@ -2,6 +2,6 @@
 sudo kubeadm init \
   --pod-network-cidr=172.16.0.0/16 \               # !!! Important this subnet cant overlap your subnet 
   --cri-socket unix:///run/containerd/containerd.sock \         # for containerd runtime
-  --control-plane-endpoint 192.168.1.110 \          # in my case that was my endpoint but you can specify any other ip like ha ip
+  --control-plane-endpoint 192.168.1.231 \          # in my case that was my endpoint but you can specify any other ip like ha ip
   --upload-certs \
   --dry-run
