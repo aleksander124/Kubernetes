b4bb0c6808c1b26c2f510a1e07877a35c5b45495
Aleksander Okon <aleksander.okon@medicover.com>
2022-10-27 13:16:49 +0200

    add resource quota script

--- scripts/resourcequot.sh
+++ scripts/resourcequot.sh
@@ -1,6 +1,44 @@
-#! /bin/bash
-echo "Wybierz jaką wartość chcesz dostać: "
+#!/bin/bash
 echo "1. Request CPU"
 echo "2. Request memory"
 echo "3. Limit CPU"
 echo "4. Limit memory"
+echo "5. End the script"
+
+
+main () {
+WARTOSC="$9"
+kubectl get resourcequotas -A | awk '{print $9}' | grep -o -P '(?<=/).*(?=,)' | awk '{s+=$1} END {printf s}'
+}
+
+command () {
+echo -n "Choose your value: "
+read -p "" VALUE
+echo "Chosen value: $VALUE "
+
+if [ "$VALUE" -eq 1 ]
+then
+kubectl get resourcequotas -A | awk '{print $9}' | grep -o -P '(?<=/).*(?=,)' | awk '{s+=$1} END {printf s}'
+
+elif [ "$VALUE" -eq 2 ]
+then
+kubectl get resourcequotas -A | awk '{print $13}' | grep -o -P '(?<=/).*(?=,)' | awk '{s+=$1} END {printf s}'
+
+elif [ "$VALUE" -eq 3 ]
+then
+kubectl get resourcequotas -A | awk '{print $17}' | grep -o -P '(?<=/).*(?=,)' | awk '{s+=$1} END {printf s}'
+
+elif [ "$VALUE" -eq 4 ]
+then
+kubectl get resourcequotas -A | awk '{print $19}' | grep -o -P '(?<=/).*(?=,)' | awk '{s+=$1} END {printf s}'
+
+elif [ "$VALUE" -eq 5 ]
+then
+echo "End of script"
+
+fi
+}
+
+command
+
+
